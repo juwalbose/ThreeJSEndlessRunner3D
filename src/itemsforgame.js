d8faa0c0774b08486586866dd2d34701ba42506b
Juwal Bose <juwalbose@gmail.com>
2017-06-22 15:54:27 +0000

    tree created from cone primitive

---
+++ src/itemsforgame.js
@@ -0,0 +1,169 @@
+/*global THREE*/
+window.addEventListener('load', init, false);
+
+var sceneWidth;
+var sceneHeight;
+var camera;
+var scene;
+var renderer;
+var dom;
+var sun;
+var ground;
+var orbitControl;
+
+function init() {
+	// set up the scene
+	createScene();
+
+	//call game loop
+	update();
+}
+
+function createScene(){
+    sceneWidth=window.innerWidth;
+    sceneHeight=window.innerHeight;
+    scene = new THREE.Scene();//the 3d scene
+    scene.fog = new THREE.FogExp2( 0xccffcc, 0.06 );
+    camera = new THREE.PerspectiveCamera( 60, sceneWidth / sceneHeight, 0.1, 1000 );//perspective camera
+    renderer = new THREE.WebGLRenderer({alpha:true});//renderer with transparent backdrop
+    renderer.shadowMap.enabled = true;//enable shadow
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.setSize( sceneWidth, sceneHeight );
+    dom = document.getElementById('TutContainer');
+	dom.appendChild(renderer.domElement);
+	
+	//add items to scene
+	var numTrees=50;
+	var newTree;
+	for(var i=0;i<numTrees;i++){
+		newTree=createTree();
+		newTree.position.z=i*-2;
+		newTree.rotation.z=(Math.random()*(2*Math.PI/10))+-Math.PI/10;
+		scene.add(newTree);
+	}
+	var planeGeometry = new THREE.PlaneGeometry( 5, 1000, 4, 4 );
+	var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
+	ground = new THREE.Mesh( planeGeometry, planeMaterial );
+	ground.receiveShadow = true;
+	ground.castShadow=false;
+	ground.rotation.x=-Math.PI/2;
+	scene.add( ground );
+
+	camera.position.z = 4;
+	camera.position.y = 1;
+	
+	sun = new THREE.DirectionalLight( 0xffffff, 1);
+	sun.position.set( 0,4,2 );
+	sun.castShadow = true;
+	scene.add(sun);
+	//Set up shadow properties for the sun light
+	sun.shadow.mapSize.width = 256;
+	sun.shadow.mapSize.height = 256;
+	sun.shadow.camera.near = 0.5;
+	sun.shadow.camera.far = 50 ;
+	
+	orbitControl = new THREE.OrbitControls( camera, renderer.domElement );//helper to rotate around in scene
+	orbitControl.addEventListener( 'change', render );
+	//orbitControl.enableDamping = true;
+	//orbitControl.dampingFactor = 0.8;
+	orbitControl.enableZoom = false;
+	
+	//var helper = new THREE.CameraHelper( sun.shadow.camera );
+	//scene.add( helper );// enable to see the light cone
+	
+	window.addEventListener('resize', onWindowResize, false);//resize callback
+}
+function createTree(){
+	var sides=8;
+	var tiers=6;
+	var scalarMultiplier=(Math.random()*(0.25-0.1))+0.05;
+	var midPointVector= new THREE.Vector3();
+	var vertexVector= new THREE.Vector3();
+	var treeGeometry = new THREE.ConeGeometry( 0.5, 1, sides, tiers);
+	var treeMaterial = new THREE.MeshStandardMaterial( { color: 0x33ff33,shading:THREE.FlatShading  } );
+	var offset;
+	midPointVector=treeGeometry.vertices[0].clone();
+	var currentTier=0;
+	var vertexIndex;
+	blowUpTree(treeGeometry.vertices,sides,0,scalarMultiplier);
+	tightenTree(treeGeometry.vertices,sides,1);
+	blowUpTree(treeGeometry.vertices,sides,2,scalarMultiplier*1.1,true);
+	tightenTree(treeGeometry.vertices,sides,3);
+	blowUpTree(treeGeometry.vertices,sides,4,scalarMultiplier*1.2);
+	tightenTree(treeGeometry.vertices,sides,5);
+	var treeTop = new THREE.Mesh( treeGeometry, treeMaterial );
+	treeTop.castShadow=true;
+	treeTop.receiveShadow=false;
+	treeTop.position.y=0.9;
+	treeTop.rotation.y=(Math.random()*(Math.PI));
+	var treeTrunkGeometry = new THREE.CylinderGeometry( 0.1, 0.1,0.5);
+	var trunkMaterial = new THREE.MeshStandardMaterial( { color: 0x886633,shading:THREE.FlatShading  } );
+	var treeTrunk = new THREE.Mesh( treeTrunkGeometry, trunkMaterial );
+	treeTrunk.position.y=0.25;
+	var tree =new THREE.Object3D();
+	tree.add(treeTrunk);
+	tree.add(treeTop);
+	return tree;
+}
+function blowUpTree(vertices,sides,currentTier,scalarMultiplier,odd){
+	var vertexIndex;
+	var vertexVector= new THREE.Vector3();
+	var midPointVector=vertices[0].clone();
+	var offset;
+	for(var i=0;i<sides;i++){
+		vertexIndex=(currentTier*sides)+1;
+		vertexVector=vertices[i+vertexIndex].clone();
+		midPointVector.y=vertexVector.y;
+		offset=vertexVector.sub(midPointVector);
+		if(odd){
+			if(i%2===0){
+				offset.normalize().multiplyScalar(scalarMultiplier/6);
+				vertices[i+vertexIndex].add(offset);
+			}else{
+				offset.normalize().multiplyScalar(scalarMultiplier);
+				vertices[i+vertexIndex].add(offset);
+				vertices[i+vertexIndex].y=vertices[i+vertexIndex+sides].y+0.05;
+			}
+		}else{
+			if(i%2!==0){
+				offset.normalize().multiplyScalar(scalarMultiplier/6);
+				vertices[i+vertexIndex].add(offset);
+			}else{
+				offset.normalize().multiplyScalar(scalarMultiplier);
+				vertices[i+vertexIndex].add(offset);
+				vertices[i+vertexIndex].y=vertices[i+vertexIndex+sides].y+0.05;
+			}
+		}
+	}
+}
+function tightenTree(vertices,sides,currentTier){
+	var vertexIndex;
+	var vertexVector= new THREE.Vector3();
+	var midPointVector=vertices[0].clone();
+	var offset;
+	for(var i=0;i<sides;i++){
+		vertexIndex=(currentTier*sides)+1;
+		vertexVector=vertices[i+vertexIndex].clone();
+		midPointVector.y=vertexVector.y;
+		offset=vertexVector.sub(midPointVector);
+		offset.normalize().multiplyScalar(0.06);
+		vertices[i+vertexIndex].sub(offset);
+	}
+}
+
+function update(){
+    //animate
+    render();
+	requestAnimationFrame(update);//request next update
+}
+function render(){
+    renderer.render(scene, camera);//draw
+}
+function onWindowResize() {
+	//resize & align
+	sceneHeight = window.innerHeight;
+	sceneWidth = window.innerWidth;
+	renderer.setSize(sceneWidth, sceneHeight);
+	camera.aspect = sceneWidth/sceneHeight;
+	camera.updateProjectionMatrix();
+}
